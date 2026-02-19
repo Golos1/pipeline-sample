c61016434f1370fc67816a8641365dbc6ccea9a7
Golos1 <68024905+Golos1@users.noreply.github.com>
2026-02-19 12:59:49 -0500

    added babel preset as dev dependency

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,5 @@ module.exports = {
     presets: [
         ['@babel/preset-env', { targets: { node: 'current' } }],
         '@babel/preset-typescript', '@babel/preset-react'
-        // Add other presets like @babel/preset-react if needed for JSX
     ],
 };
